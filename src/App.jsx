14168c8d821cdc4c6e26135d985bd2cc524f22db
Shivani Khatri <khatri.shivani96@gmail.com>
2024-12-31 21:11:33 +0100

    quotes obtained from api instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import inspirationalQuotes from "./assets/quotes.js";
+// import inspirationalQuotes from "./assets/quotes.js";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [quoteList, setQuoteList] = useState(null);
+
+  useEffect(() => {
+    // if (quoteList === null) {
+      const fetchData = async () => {
+        const api_url = "https://go-quote.azurewebsites.net/quotes?page=1&page_size=20&format=json";
+        try {
+          const response = await fetch(api_url);
+          const data = await response.json();
+          console.log('dataaa', data.quotes);
+          setQuoteList(data.quotes);
+        } catch (err) {
+          console.log(err);
+          setQuoteList(null);
+        }
+      };
+      fetchData();
+     
+    // }
+  }, []);
+
   const [count, setCount] = useState(0);
 
   const [selectedQuote, setSelectedQuote] = useState();
@@ -23,7 +44,8 @@ function App() {
           <div>{quote}</div>
         ))}
       </div> */}
-      <button onClick={randomClick}>New Quote</button>
+      <button>New Quote</button>
+      {/* <button onClick={randomClick}>New Quote</button> */}
       <div>{selectedQuote}</div>
     </>
   );
